bdbd5d4fd328d3843882888fbcb852287771701d
Amisha <amishasahu14@gmail.com>
2021-07-12 04:34:00 +0530

    port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const socket = require("socket.io");
 const app = express();
 
 
-let server = app.listen(443, function () {
+let server = app.liste((process.env.PORT||7000), function () {
   console.log("Server is running");
 });
 
