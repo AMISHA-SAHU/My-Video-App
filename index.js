7b9c823f57d4b5604100e435145d288d3502b6e1
AMISHA-SAHU <57638563+AMISHA-SAHU@users.noreply.github.com>
2021-07-12 09:12:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ const express = require("express");
 const socket = require("socket.io");
 const app = express();
 
-
+//setting up server
 let server = app.listen((process.env.PORT||7000), function () {
   console.log("Server is running");
 });
-
+//using app
 app.use(express.static("public"));
 
-
+//socket.io with express
 let io = socket(server);
 
 
 io.on("connection", function (socket) {
   console.log("User Connected :" + socket.id);
 
-
+//create room and join
   socket.on("join", function (roomName) {
     let rooms = io.sockets.adapter.rooms;
     let room = rooms.get(roomName);
@@ -32,7 +32,7 @@ io.on("connection", function (socket) {
     }
     console.log(rooms);
   });
-
+ //to broadcast the name of each participant excluding the user joined
   socket.on("ready", function (roomName) {
     socket.broadcast.to(roomName).emit("ready"); 
   });
@@ -44,13 +44,13 @@ io.on("connection", function (socket) {
   });
 
  
-
+//create offer
   socket.on("offer", function (offer, roomName) {
     socket.broadcast.to(roomName).emit("offer", offer); 
   });
 
  
-
+//for answering the call
   socket.on("answer", function (answer, roomName) {
     socket.broadcast.to(roomName).emit("answer", answer); 
   });
